bade473439120ad0877146f345db138e01e9c67e
electrovir <electrovir@users.noreply.github.com>
2021-10-12 07:52:23 -0600

    rename deleteArrayIndexes to filterOutIndexes
    The function implementation didn't change. It was never actually _deleting_ indexes, it was always simply filerting them out.

--- src/augments/array.ts
+++ src/augments/array.ts
@@ -1,3 +1,3 @@
-export function deleteArrayIndexes<T>(array: Readonly<T[]>, indexes: Readonly<number[]>): T[] {
+export function filterOutIndexes<T>(array: Readonly<T[]>, indexes: Readonly<number[]>): T[] {
     return array.filter((_, index) => !indexes.includes(index));
 }
