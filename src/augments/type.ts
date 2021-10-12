b4c8c4d4db5a8253383007fb54d34bb51514c790
electrovir <electrovir@users.noreply.github.com>
2021-10-12 07:53:21 -0600

    add more type augments and comments
    Each exported type augment now has its own JSDoc-style comment.
    Added:
    ArrayElement
    RequiredAndNotNull
    RequiredBy
    RequiredAndNotNullBy

--- src/augments/type.ts
+++ src/augments/type.ts
@@ -1,3 +1,29 @@
+/** Makes all properties in an object writable. This is the opposite of Readonly<> */
 export type Writeable<T> = {-readonly [P in keyof T]: T[P]};
+
+/** Makes all property values in an object also readonly. Can cause issues on primitive. */
 export type DeepWriteable<T> = {-readonly [P in keyof T]: DeepWriteable<T[P]>};
+
+/** Replace properties in T with properties in U. */
 export type Overwrite<T, U> = Pick<T, Exclude<keyof T, keyof U>> & U;
+
+/** Extract the element type out of an array type. */
+export type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType[number];
+
+/**
+ * Same as the Required<> built-in type helper but this requires that each property be present and
+ * be not null.
+ */
+export type RequiredAndNotNull<T> = {
+    [P in keyof T]-?: NonNullable<T[P]>;
+};
+
+/** Require only a subset of object properties. */
+export type RequiredBy<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
+
+/**
+ * Require only a subset of object properties and require that they be not null. This is
+ * particularly useful in conjunction with the "exactOptionalPropertyTypes" tsconfig flag.
+ */
+export type RequiredAndNotNullBy<T, K extends keyof T> = Omit<T, K> &
+    Required<{[PropertyName in K]: NonNullable<T[PropertyName]>}>;
