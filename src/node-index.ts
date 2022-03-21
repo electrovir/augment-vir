f055c1698b94dd3c6cd720bd3851ac8e9227f57d
electrovir <electrovir@users.noreply.github.com>
2022-03-20 20:10:10 -0600

    exclude web-index from node-index

--- src/node-index.ts
+++ src/node-index.ts
@@ -3,4 +3,3 @@ export * from './augments/node-only/file-system';
 export {randomString} from './augments/node-only/node-string';
 export * from './augments/node-only/path';
 export * from './augments/node-only/shell';
-export * from './web-index';
