c120a81a3d69e7ca22b0c967d129caa49fb2ff0a
electrovir <electrovir@users.noreply.github.com>
2022-04-09 14:31:49 -0600

    add jest augments to exports

--- src/node-only.ts
+++ src/node-only.ts
@@ -1,5 +1,6 @@
 /** Node.js-only augments only */
 export * from './augments/node-only/file-system';
-export {randomString} from './augments/node-only/node-string';
+export * from './augments/node-only/jest';
+export * from './augments/node-only/node-string';
 export * from './augments/node-only/path';
 export * from './augments/node-only/shell';
