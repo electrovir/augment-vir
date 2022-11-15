6729c4adc13509dae0a21dffaa02fae3493875f6
electrovir <electrovir@users.noreply.github.com>
2022-11-15 22:58:32 +0000

    export new node-js augments from the last commit

--- packages/node-js/src/index.ts
+++ packages/node-js/src/index.ts
@@ -1,5 +1,7 @@
 export * from './augments/console';
 export * from './augments/file-system';
+export * from './augments/json';
+export * from './augments/number';
 export * from './augments/path';
 export * from './augments/shell';
 export * from './augments/string';
