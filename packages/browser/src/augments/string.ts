02876945eb77b7a7dcc6686d10795f8e21cdd48e
electrovir <electrovir@users.noreply.github.com>
2022-12-03 18:38:03 +0000

    use self instead of window in random string
    so it works in web workers

--- packages/browser/src/augments/string.ts
+++ packages/browser/src/augments/string.ts
@@ -1,13 +1,13 @@
 export function randomString(inputLength: number = 16): string {
     const arrayLength = Math.ceil(inputLength / 2);
     // server side
-    if (typeof window === 'undefined' || !window.crypto) {
+    if (typeof self === 'undefined' || !self.crypto) {
         throw new Error(
-            `window/window.crypto is not defined for the "${randomString.name}" function. If using this in a Node.js context, import ${randomString.name} from 'augment-vir/node-js' instead`,
+            `self/self.crypto is not defined for the "${randomString.name}" function. If using this in a Node.js context, import ${randomString.name} from 'augment-vir/node-js' instead`,
         );
     }
     const uintArray = new Uint8Array(arrayLength);
-    window.crypto.getRandomValues(uintArray);
+    self.crypto.getRandomValues(uintArray);
     return (
         Array.from(uintArray)
             .map((value) => value.toString(16).padStart(2, '0'))
