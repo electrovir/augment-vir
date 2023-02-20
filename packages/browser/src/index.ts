a47b7f3495a2934fa205eab098ca7329d090d3da
electrovir <electrovir@users.noreply.github.com>
2023-02-20 14:26:18 +0000

    export new queryThroughShadow

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -1,5 +1,6 @@
 export * from './augments/browser-string';
 export * from './augments/image';
 export * from './augments/pixel';
+export * from './augments/query-through-shadow';
 export * from './augments/search-params';
 export * from './augments/video';
