9749882fb826a7b786840d0fad3eff2b7414f780
electrovir <electrovir@users.noreply.github.com>
2023-04-02 13:29:04 +0000

    don't modify chai's truncateThreshold in itCases

--- packages/chai/src/augments/chai-it-cases.ts
+++ packages/chai/src/augments/chai-it-cases.ts
@@ -1,13 +1,11 @@
 import {AnyFunction} from '@augment-vir/common';
 import {FunctionTestCase, itCases as generic_itCases} from '@augment-vir/testing';
-import chai, {assert} from 'chai';
+import {assert} from 'chai';
 export type {
     FunctionTestCase,
     OutputTestCaseMultipleInputs,
     OutputTestCaseSingleInput,
 } from '@augment-vir/testing';
-
-chai.config.truncateThreshold = 0;
 
 export const runItCases = itCases;
 
