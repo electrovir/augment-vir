e29ef19ffcc15d09cdc33e21e9ef1451a330e809
electrovir <electrovir@users.noreply.github.com>
2023-05-18 20:29:31 +0000

    remove itSnapshots, it has been moved to the test-established-expectations package

--- packages/chai/src/index.ts
+++ packages/chai/src/index.ts
@@ -2,4 +2,3 @@ export * from './augments/chai-assert';
 export * from './augments/chai-assert-output';
 export * from './augments/chai-assert-type-of';
 export * from './augments/chai-it-cases';
-export * from './augments/it-snapshots';
