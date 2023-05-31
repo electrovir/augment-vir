7e934cbde7b93f9621bdf8eb90035ee9b669f868
electrovir <electrovir@users.noreply.github.com>
2023-05-31 11:54:26 +0000

    upgrade deps

--- packages/testing/src/augments/assert-type-of.ts
+++ packages/testing/src/augments/assert-type-of.ts
@@ -1,23 +1,34 @@
 import {Overwrite} from '@augment-vir/common';
-import {ExpectTypeOf, Not} from 'expect-type';
+import {ExpectTypeOf, ExpectTypeOfOptions, Not} from 'expect-type';
 
-type ExtraTypeChecks<TestingType, B extends boolean> = {
+type ExtraTypeChecks<TestingType, B extends ExpectTypeOfOptions> = {
     toBeAssignableTo: ExpectTypeOf<TestingType, B>['toMatchTypeOf'];
 };
 
-type WithExtraNotTypeChecks<TestingType, B extends boolean> = Overwrite<
+type WithExtraNotTypeChecks<TestingType, B extends ExpectTypeOfOptions> = Overwrite<
     ExpectTypeOf<TestingType, B>,
     {
-        not: ExpectTypeOf<TestingType, B>['not'] & ExtraTypeChecks<TestingType, Not<B>>;
+        not: ExpectTypeOf<TestingType, B>['not'] &
+            ExtraTypeChecks<TestingType, B & {positive: Not<B['positive']>}>;
     }
 >;
 
-type AssertTypeOf<TestingType, B extends boolean> = WithExtraNotTypeChecks<TestingType, B> &
+type AssertTypeOf<TestingType, B extends ExpectTypeOfOptions> = WithExtraNotTypeChecks<
+    TestingType,
+    B
+> &
     ExtraTypeChecks<TestingType, B>;
 
-export function assertTypeOf<TestingType>(input: TestingType): AssertTypeOf<TestingType, true>;
-export function assertTypeOf<TestingType>(): AssertTypeOf<TestingType, true>;
-export function assertTypeOf<TestingType>(input?: TestingType): AssertTypeOf<TestingType, true> {
+export function assertTypeOf<TestingType>(
+    input: TestingType,
+): AssertTypeOf<TestingType, {branded: false; positive: true}>;
+export function assertTypeOf<TestingType>(): AssertTypeOf<
+    TestingType,
+    {branded: false; positive: true}
+>;
+export function assertTypeOf<TestingType>(
+    input?: TestingType,
+): AssertTypeOf<TestingType, {branded: false; positive: true}> {
     // run time implementation for this doesn't matter
     const runtimeProxy = new Proxy(
         {},
