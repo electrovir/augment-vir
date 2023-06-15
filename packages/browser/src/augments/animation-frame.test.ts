2d141ac53178da22076b72e8fbda4d4e32eaeaaf
electrovir <electrovir@users.noreply.github.com>
2023-06-15 19:05:56 +0000

    fix timing issue in CI tests

--- packages/browser/src/augments/animation-frame.test.ts
+++ packages/browser/src/augments/animation-frame.test.ts
@@ -5,9 +5,10 @@ import {waitForAnimationFrame} from './animation-frame';
 describe(waitForAnimationFrame.name, () => {
     it('waits for a non-zero amount of time', async () => {
         const singleFrameDuration = await timeCallback(() => waitForAnimationFrame(2));
-        const multipleFrameDuration = await timeCallback(() => waitForAnimationFrame(5));
+        const multipleFrameDuration = await timeCallback(() => waitForAnimationFrame(10));
 
+        /** These tests are intentionally lenient: CI runners have issues with timing. */
         assert.isAbove(singleFrameDuration, 0);
-        assert.isAbove(multipleFrameDuration, singleFrameDuration * 2);
+        assert.isAbove(multipleFrameDuration, singleFrameDuration);
     });
 });
