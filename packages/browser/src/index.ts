37ea347981ebbf77cb3a63e94c04155e508c183e
electrovir <electrovir@users.noreply.github.com>
2023-06-24 16:30:51 +0000

    add ElementPosition type and checkIfEntirelyInScrollView, getCenterOfElement, appendPositionDebugDiv functions to @augment-vir/browser

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -1,6 +1,7 @@
 export * from './augments/animation-frame';
 export * from './augments/browser-array';
 export * from './augments/browser-random';
+export * from './augments/elements/element-position';
 export * from './augments/event-target';
 export * from './augments/image';
 export * from './augments/query-through-shadow';
