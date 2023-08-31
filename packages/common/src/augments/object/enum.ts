083c09417c8a990568756b102ec0dec77fb529fe
electrovir <electrovir@users.noreply.github.com>
2023-08-31 12:46:46 +0000

    clarify base enum type
    use Record<string, number | string> instead of just object

--- packages/common/src/augments/object/enum.ts
+++ packages/common/src/augments/object/enum.ts
@@ -1,24 +1,29 @@
 import {getObjectTypedKeys} from './object-entries';
 
-export function getEnumTypedKeys<T extends object>(input: T): (keyof T)[] {
+export type EnumBaseType = Record<string, number | string>;
+
+export function getEnumTypedKeys<T extends EnumBaseType>(input: T): (keyof T)[] {
     // enum keys are always strings
     return getObjectTypedKeys(input).filter((key) => isNaN(Number(key))) as (keyof T)[];
 }
 
-export function getEnumTypedValues<T extends object>(input: T): T[keyof T][] {
+export function getEnumTypedValues<T extends EnumBaseType>(input: T): T[keyof T][] {
     const keys = getEnumTypedKeys(input);
     return keys.map((key) => input[key]);
 }
 
 /** Check if the given value is within the given enum. */
-export function isEnumValue<T extends object>(input: unknown, checkEnum: T): input is T[keyof T] {
+export function isEnumValue<T extends EnumBaseType>(
+    input: unknown,
+    checkEnum: T,
+): input is T[keyof T] {
     return getEnumTypedValues(checkEnum).includes(input as T[keyof T]);
 }
 
 /** Interpret a primitive as an enum value with type safety. */
 export function ensureEnum<
     const ValueType extends `${EnumType[keyof EnumType]}`,
-    const EnumType extends object,
+    const EnumType extends EnumBaseType,
 >(value: ValueType, checkEnum: EnumType): EnumType[keyof EnumType] {
     if (isEnumValue(value, checkEnum)) {
         return value;
@@ -32,7 +37,7 @@ export function ensureEnum<
     }
 }
 
-export function filterToEnumValues<T extends object>(
+export function filterToEnumValues<T extends EnumBaseType>(
     inputs: ReadonlyArray<unknown>,
     checkEnum: T,
     caseInsensitive = false,
