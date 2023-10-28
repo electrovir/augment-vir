46100954f52882d8bd7d3325b1a99533ddbf1620
electrovir <electrovir@users.noreply.github.com>
2023-10-28 12:16:35 +0000

    add round function to @augment-vir/common

--- packages/common-tests/src/tests/number.test.ts
+++ packages/common-tests/src/tests/number.test.ts
@@ -1,5 +1,5 @@
 import {itCases} from '@augment-vir/chai';
-import {toEnsuredNumber} from '@augment-vir/common';
+import {round, toEnsuredNumber} from '@augment-vir/common';
 
 describe(toEnsuredNumber.name, () => {
     itCases(toEnsuredNumber, [
@@ -17,6 +17,43 @@ describe(toEnsuredNumber.name, () => {
             it: 'errors on object input',
             input: {},
             throws: Error,
+        },
+    ]);
+});
+
+describe(round.name, () => {
+    itCases(round, [
+        {
+            it: 'rounds down with 2 decimals',
+            input: {
+                digits: 2,
+                number: 1.123456,
+            },
+            expect: 1.12,
+        },
+        {
+            it: 'rounds up with 2 decimals',
+            input: {
+                digits: 2,
+                number: 1.125456,
+            },
+            expect: 1.13,
+        },
+        {
+            it: 'rounds up with 3 decimals and a carry',
+            input: {
+                digits: 3,
+                number: 1.129556,
+            },
+            expect: 1.13,
+        },
+        {
+            it: 'rounds up with 0 decimals',
+            input: {
+                digits: 0,
+                number: 1.564123,
+            },
+            expect: 2,
         },
     ]);
 });
--- packages/common/src/augments/number.ts
+++ packages/common/src/augments/number.ts
@@ -17,3 +17,10 @@ export function wrapNumber({max, min, value}: {value: number; max: number; min: 
 
     return value;
 }
+
+export function round(inputs: {number: number; digits: number}): number {
+    const digitFactor = Math.pow(10, inputs.digits);
+    const multiplied = inputs.number * digitFactor;
+
+    return Number((Math.round(multiplied) / digitFactor).toFixed(inputs.digits));
+}
