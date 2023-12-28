04e35e96c096798f38a713c4b35af5ae51beb990
electrovir <electrovir@users.noreply.github.com>
2023-12-28 15:07:42 +0000

    [minor] add calculateTextDimensions to @augment-vir/browser

---
+++ packages/browser/src/augments/elements/text-width.test.ts
@@ -0,0 +1,122 @@
+import {Dimensions, awaitedBlockingMap, mapObjectValues} from '@augment-vir/common';
+import {assert, fixture} from '@open-wc/testing';
+import {CSSResult, css, html} from 'element-vir';
+import {isRunTimeType} from 'run-time-assertions';
+import {calculateTextDimensions} from './text-width';
+
+describe(calculateTextDimensions.name, () => {
+    async function testCalculateTextDimensions(
+        text: string,
+        customStyles: CSSResult | undefined = css``,
+    ): Promise<Dimensions> {
+        const parentElement = await fixture(html`
+            <div
+                style=${css`
+                    ${customStyles}
+                    display: inline-block;
+                `}
+            ></div>
+        `);
+
+        const rawDimensions = await calculateTextDimensions(parentElement, text);
+
+        return mapObjectValues(rawDimensions, (key, value) => Math.round(value));
+    }
+
+    const testCases: ReadonlyArray<{
+        name: string;
+        text: string;
+        customStyle?: CSSResult | undefined;
+        expect: {
+            width: number | {min: number; max: number};
+            height: number | {min: number; max: number};
+        };
+    }> = [
+        {
+            name: 'empty string',
+            text: '',
+            expect: {
+                height: 0,
+                width: 0,
+            },
+        },
+        {
+            name: 'simple text',
+            text: 'hello there',
+            expect: {
+                height: {
+                    min: 17,
+                    max: 20,
+                },
+                width: 68,
+            },
+        },
+        {
+            name: 'wrapped long text',
+            text: 'hello there really long text super duper long why is it so long oh no it is never going to end hello there really long text super duper long why is it so long oh no it is never going to end hello there really long text super duper long why is it so long oh no it is never going to end hello there really long text super duper long why is it so long oh no it is never going to end hello there really long text super duper long why is it so long oh no it is never going to end hello there really long text super duper long why is it so long oh no it is never going to end hello there really long text super duper long why is it so long oh no it is never going to end',
+            expect: {
+                height: {
+                    min: 107,
+                    max: 116,
+                },
+                width: 792,
+            },
+        },
+        {
+            name: 'unwrapped long text',
+            text: 'hello there really long text super duper long why is it so long oh no it is never going to end hello there really long text super duper long why is it so long oh no it is never going to end hello there really long text super duper long why is it so long oh no it is never going to end hello there really long text super duper long why is it so long oh no it is never going to end hello there really long text super duper long why is it so long oh no it is never going to end hello there really long text super duper long why is it so long oh no it is never going to end hello there really long text super duper long why is it so long oh no it is never going to end',
+            customStyle: css`
+                white-space: nowrap;
+            `,
+            expect: {
+                height: {
+                    min: 17,
+                    max: 20,
+                },
+                width: 4093,
+            },
+        },
+    ];
+
+    it('passes all test cases', async () => {
+        await awaitedBlockingMap(testCases, async (testCase) => {
+            const output = await testCalculateTextDimensions(testCase.text, testCase.customStyle);
+            if (isRunTimeType(testCase.expect.width, 'number')) {
+                assert.strictEqual(
+                    output.width,
+                    testCase.expect.width,
+                    `Incorrect exact width for '${testCase.name}' test case`,
+                );
+            } else {
+                assert.isAbove(
+                    output.width,
+                    testCase.expect.width.min,
+                    `Incorrect min width for '${testCase.name}' test case`,
+                );
+                assert.isBelow(
+                    output.width,
+                    testCase.expect.width.max,
+                    `Incorrect max width for '${testCase.name}' test case`,
+                );
+            }
+            if (isRunTimeType(testCase.expect.height, 'number')) {
+                assert.strictEqual(
+                    output.height,
+                    testCase.expect.height,
+                    `Incorrect exact height for '${testCase.name}' test case`,
+                );
+            } else {
+                assert.isAbove(
+                    output.height,
+                    testCase.expect.height.min,
+                    `Incorrect min height for '${testCase.name}' test case`,
+                );
+                assert.isBelow(
+                    output.height,
+                    testCase.expect.height.max,
+                    `Incorrect max height for '${testCase.name}' test case`,
+                );
+            }
+        });
+    });
+});
---
+++ packages/browser/src/augments/elements/text-width.ts
@@ -0,0 +1,63 @@
+import {Dimensions, PartialAndUndefined, waitForCondition} from '@augment-vir/common';
+
+export async function calculateTextDimensions(
+    parentElement: Element,
+    text: string,
+    customOptions?: PartialAndUndefined<{
+        timeout: {milliseconds: number};
+        errorMessage: string;
+    }>,
+): Promise<Dimensions> {
+    if (!text) {
+        return {
+            width: 0,
+            height: 0,
+        };
+    }
+    const textWrapperElement = document.createElement('span');
+    textWrapperElement.style.visibility = 'hidden';
+    textWrapperElement.style.position = 'absolute';
+    textWrapperElement.style.zIndex = '-9999';
+    textWrapperElement.style.opacity = '0';
+    textWrapperElement.style.pointerEvents = 'none';
+    textWrapperElement.style.userSelect = 'none';
+    textWrapperElement.style.webkitUserSelect = 'none';
+    textWrapperElement.style.animation = 'none';
+    textWrapperElement.style.transition = 'none';
+    textWrapperElement.setAttribute('aria-hidden', 'true');
+
+    parentElement.appendChild(textWrapperElement);
+    let latestSize: DOMRectReadOnly | undefined;
+    /**
+     * Use resize observer instead of clientWidth, getComputedStyle, or getBoundingClientRect to
+     * prevent a forced re-layout. See https://gist.github.com/paulirish/5d52fb081b3570c81e3a.
+     */
+    const resizeObserver = new ResizeObserver((entries) => {
+        latestSize = entries[0]?.contentRect;
+    });
+    resizeObserver.observe(textWrapperElement);
+    textWrapperElement.innerText = text;
+
+    try {
+        await waitForCondition({
+            conditionCallback() {
+                return !!latestSize;
+            },
+            intervalMs: 100,
+            timeoutMs: customOptions?.timeout?.milliseconds || 10_000,
+            timeoutMessage: customOptions?.errorMessage || 'Failed to calculate text size.',
+        });
+
+        if (!latestSize) {
+            throw new Error('Failed to calculate text size.');
+        }
+
+        return {
+            height: latestSize.height,
+            width: latestSize.width,
+        };
+    } finally {
+        textWrapperElement.remove();
+        resizeObserver.disconnect();
+    }
+}
