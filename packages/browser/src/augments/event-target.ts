6a485146664fa5315ae7fb72c6f10e5ea738faa4
electrovir <electrovir@users.noreply.github.com>
2024-03-09 14:32:56 +0000

    [patch] change useDefaultTarget intpu to extractEventTarget to an object of options in @augment-vir/browser

--- packages/browser/src/augments/event-target.ts
+++ packages/browser/src/augments/event-target.ts
@@ -1,16 +1,28 @@
+import {PartialAndUndefined} from '@augment-vir/common';
 import {Constructor} from 'type-fest';
 
 export function extractEventTarget<ExpectedTargetClassConstructor extends Constructor<Element>>(
     event: Event,
     expectedTargetClass: ExpectedTargetClassConstructor,
-    useDefaultTarget?: boolean,
+    options: PartialAndUndefined<{
+        /**
+         * By default `extractEventTarget` uses the `Event.currentTarget` field to extract the event
+         * target so that it extract the target to which the event handler was attached to. However,
+         * if you wish to instead use the `Event.target` property, to extract the element which
+         * originally fired the event, set this to true.
+         */
+        useOriginalTarget: boolean;
+    }> = {},
 ): InstanceType<ExpectedTargetClassConstructor> {
-    const target = useDefaultTarget ? event.target : event.currentTarget;
+    const target = options.useOriginalTarget ? event.target : event.currentTarget;
 
     if (!(target instanceof expectedTargetClass)) {
-        throw new Error(
-            `Target from event '${event.type}' was not of type '${expectedTargetClass.constructor.name}'. Got '${target?.constructor.name}'.`,
-        );
+        const expectedName = expectedTargetClass.name;
+        const actualName = target?.constructor.name;
+        const message = options.useOriginalTarget
+            ? `Current target from event '${event.type}' was not of type '${expectedName}'. Got '${actualName}'.`
+            : `Target from event '${event.type}' was not of type '${expectedName}'. Got '${actualName}'.`;
+        throw new Error(message);
     }
 
     return target as InstanceType<ExpectedTargetClassConstructor>;
