01dec2bca18c331eceb185cd99daad3dd17f381c
electrovir <electrovir@users.noreply.github.com>
2024-03-28 14:43:03 +0000

    [patch] fix RemoveFirstTupleEntry

--- packages/common/src/augments/tuple.ts
+++ packages/common/src/augments/tuple.ts
@@ -51,9 +51,9 @@ export type MappedTuple<Tuple extends ReadonlyArray<any>, NewValueType> = {
 
 export type MaybeTuple<T> = T | AtLeastTuple<T, 1>;
 
-export type RemoveFirstTupleEntry<T extends ReadonlyArray<unknown>> = T extends [
+export type RemoveFirstTupleEntry<T extends ReadonlyArray<unknown>> = T extends readonly [
     firstArg: unknown,
     ...theRest: infer TheRest,
 ]
     ? TheRest
-    : never;
+    : T;
