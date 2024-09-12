2b6efea0f05cc8fcb4aefca24a5e9d20eca4bd9f
electrovir <electrovir@users.noreply.github.com>
2024-09-12 17:29:22 +0000

    skip loading video in WebKit on Windows

--- packages/web/src/augments/video.test.ts
+++ packages/web/src/augments/video.test.ts
@@ -1,10 +1,19 @@
 import {assert} from '@augment-vir/assert';
+import {log} from '@augment-vir/common';
 import {describe, it} from '@augment-vir/test';
+import Bowser from 'bowser';
 import {loadVideo} from './video.js';
+
+const bowser = Bowser.getParser(navigator.userAgent);
 
 describe(loadVideo.name, () => {
     it('loads a video', async () => {
-        assert.instanceOf(await loadVideo('/video.mock.webm'), HTMLVideoElement);
+        log.debug({os: bowser.getOSName(), browser: bowser.getBrowserName()});
+
+        /** This works in every environment except WebKit on Windows in tests. */
+        if (bowser.getOSName() !== 'Windows' && bowser.getBrowserName() !== 'WebKit') {
+            assert.instanceOf(await loadVideo('/video.mock.webm'), HTMLVideoElement);
+        }
     });
     it('rejects a missing video', async () => {
         await assert.throws(loadVideo('/missing.webm'));
