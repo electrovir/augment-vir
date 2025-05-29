303e6b656dc2569f78dc553ccdbb7aca72b85e6f
electrovir <electrovir@users.noreply.github.com>
2025-05-29 00:34:24 +0000

    [minor] allow wrapNumber to consider overflow

--- packages/common/src/augments/number/wrap-number.test.ts
+++ packages/common/src/augments/number/wrap-number.test.ts
@@ -1,4 +1,5 @@
-import {describe, itCases} from '@augment-vir/test';
+import {assert} from '@augment-vir/assert';
+import {describe, it, itCases} from '@augment-vir/test';
 import {wrapNumber} from './wrap-number.js';
 
 describe(wrapNumber.name, () => {
@@ -27,5 +28,145 @@ describe(wrapNumber.name, () => {
             ],
             expect: 5,
         },
+        {
+            it: 'allows overflowing positive with min = 0',
+            inputs: [
+                13,
+                {min: 0, max: 3, takeOverflow: true},
+            ],
+            expect: 1,
+        },
+        {
+            it: 'allows overflowing negative with min = 0',
+            inputs: [
+                -7,
+                {min: 0, max: 3, takeOverflow: true},
+            ],
+            expect: 1,
+        },
+        {
+            it: 'allows overflowing positive with min > 0',
+            inputs: [
+                7,
+                {min: 2, max: 5, takeOverflow: true},
+            ],
+            expect: 3,
+        },
+        {
+            it: 'allows overflowing negative with min > 0',
+            inputs: [
+                -7,
+                {min: 2, max: 5, takeOverflow: true},
+            ],
+            expect: 5,
+        },
+        {
+            it: 'allows overflowing positive with min < 0',
+            inputs: [
+                7,
+                {min: -2, max: 5, takeOverflow: true},
+            ],
+            expect: -1,
+        },
+        {
+            it: 'allows overflowing negative with min < 0',
+            inputs: [
+                -10,
+                {min: -2, max: 5, takeOverflow: true},
+            ],
+            expect: -2,
+        },
+        {
+            it: 'ignores non wrapping value with overflow',
+            inputs: [
+                7,
+                {min: 0, max: 10, takeOverflow: true},
+            ],
+            expect: 7,
+        },
     ]);
+    it('allows sequencing through all values', () => {
+        const positiveWrapping: number[] = [];
+        const positiveWrapNoOverflow: number[] = [];
+        const negativeWrapping: number[] = [];
+        const negativeWrapNoOverflow: number[] = [];
+
+        for (let i = 0; i < 12; i++) {
+            positiveWrapping.push(wrapNumber(i, {min: 0, max: 4, takeOverflow: true}));
+            positiveWrapNoOverflow.push(wrapNumber(i, {min: 0, max: 4}));
+        }
+        for (let i = 0; i > -12; i--) {
+            negativeWrapping.push(wrapNumber(i, {min: 0, max: 4, takeOverflow: true}));
+            negativeWrapNoOverflow.push(wrapNumber(i, {min: 0, max: 4}));
+        }
+
+        assert.deepEquals(
+            positiveWrapping,
+            [
+                0,
+                1,
+                2,
+                3,
+                4,
+                0,
+                1,
+                2,
+                3,
+                4,
+                0,
+                1,
+            ],
+        );
+        assert.deepEquals(
+            positiveWrapNoOverflow,
+            [
+                0,
+                1,
+                2,
+                3,
+                4,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+            ],
+        );
+        assert.deepEquals(
+            negativeWrapping,
+            [
+                0,
+                4,
+                3,
+                2,
+                1,
+                0,
+                4,
+                3,
+                2,
+                1,
+                0,
+                4,
+            ],
+        );
+        assert.deepEquals(
+            negativeWrapNoOverflow,
+            [
+                0,
+                4,
+                4,
+                4,
+                4,
+                4,
+                4,
+                4,
+                4,
+                4,
+                4,
+                4,
+            ],
+        );
+    });
 });
--- packages/common/src/augments/number/wrap-number.ts
+++ packages/common/src/augments/number/wrap-number.ts
@@ -1,8 +1,11 @@
-import {ensureMinMax, type MinMax} from '@augment-vir/core';
+import {ensureMinMax, type MinMax, type PartialWithUndefined} from '@augment-vir/core';
 
 /**
  * If the given value is outside the given min/max bounds, instead of clamping the number (as the
  * `clamp` function does), this function wraps the value around to the next bound (inclusive).
+ *
+ * If `takeOverflow` is set to `true`, then any excess after wrapping is continually iterated over
+ * and wrapped again, potentially resulting in wrapping through the min/max bounds multiple times.
  *
  * @category Number
  * @category Package : @augment-vir/common
@@ -11,20 +14,45 @@ import {ensureMinMax, type MinMax} from '@augment-vir/core';
  * ```ts
  * import {wrapNumber} from '@augment-vir/common';
  *
- * wrapNumber({min: 0, max: 100, value: 101}); // 0
- * wrapNumber({min: 0, max: 100, value: -1}); // 100
+ * wrapNumber(101, {min: 0, max: 100}); // 0
+ * wrapNumber(-1, {min: 0, max: 100}); // 100
+ * wrapNumber(-7, {min: 0, max: 3, takeOverflow: true}); // 1
+ * wrapNumber(13, {min: 0, max: 3, takeOverflow: true}); // 1
  * ```
  *
  * @package [`@augment-vir/common`](https://www.npmjs.com/package/@augment-vir/common)
  */
-export function wrapNumber(value: number, minMax: Readonly<MinMax>): number {
+export function wrapNumber(
+    value: number,
+    minMax: Readonly<MinMax> &
+        PartialWithUndefined<{
+            /**
+             * If `true`, the overflow of the wrap is added to the bound wrapped to.
+             *
+             * @default false
+             */
+            takeOverflow: boolean;
+        }>,
+): number {
     const {min, max} = ensureMinMax(minMax);
 
-    if (value > max) {
-        return min;
-    } else if (value < min) {
-        return max;
-    }
+    if (minMax.takeOverflow) {
+        const diff = max - min + 1;
 
-    return value;
+        const offset = (value - min) % diff;
+
+        if (offset < 0) {
+            return min + diff + offset;
+        } else {
+            return min + offset;
+        }
+    } else {
+        if (value > max) {
+            return min;
+        } else if (value < min) {
+            return max;
+        }
+
+        return value;
+    }
 }
