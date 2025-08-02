bc7b051d9ffb88fcba6291d1dac26493230e6851
electrovir <electrovir@users.noreply.github.com>
2025-08-02 21:19:07 +0000

    [patch] fix PrismaFullModel for possibly undefined nested models

--- packages/common/src/augments/prisma/prisma-full-model.ts
+++ packages/common/src/augments/prisma/prisma-full-model.ts
@@ -1,17 +1,40 @@
 import {type BaseTypeMap} from './base-prisma-types.js';
 import {type PrismaBasicModel} from './prisma-basic-model.js';
 
+/**
+ * Gets the full model type with all nested models for Prisma.
+ *
+ * @category Prisma : Common
+ * @category Package : @augment-vir/common
+ * @package [`@augment-vir/common`](https://www.npmjs.com/package/@augment-vir/common)
+ */
 export type PrismaFullModel<
     TypeMap extends BaseTypeMap,
     Model extends keyof TypeMap['model'],
-> = PrismaBasicModel<TypeMap, Model> & ExpandObjects<TypeMap['model'][Model]['payload']['objects']>;
+> = PrismaBasicModel<TypeMap, Model> &
+    ExpandModelObjects<TypeMap['model'][Model]['payload']['objects'], TypeMap>;
 
-type ExpandObjects<Objects> = {
-    [K in keyof Objects]: Objects[K] extends (infer U)[]
-        ? U extends {scalars: any; objects: any}
-            ? Array<U['scalars'] & ExpandObjects<U['objects']>>
-            : Objects[K]
-        : Objects[K] extends {scalars: any; objects: any}
-          ? Objects[K]['scalars'] & ExpandObjects<Objects[K]['objects']>
-          : Objects[K];
+/**
+ * Expand a model's objects (nested models).
+ *
+ * @category Prisma : Common
+ * @category Package : @augment-vir/common
+ * @package [`@augment-vir/common`](https://www.npmjs.com/package/@augment-vir/common)
+ */
+export type ExpandModelObjects<Objects, TypeMap extends BaseTypeMap> = {
+    [Key in keyof Objects]: Objects[Key] extends ReadonlyArray<infer Entry>
+        ? ExpandModel<TypeMap, Entry>[]
+        : ExpandModel<TypeMap, Objects[Key]>;
 };
+
+/**
+ * Expand a model entry from {@link ExpandModelObjects}.
+ *
+ * @category Prisma : Common
+ * @category Package : @augment-vir/common
+ * @package [`@augment-vir/common`](https://www.npmjs.com/package/@augment-vir/common)
+ */
+export type ExpandModel<TypeMap extends BaseTypeMap, Entry> =
+    Entry extends Readonly<{name: infer ModelName extends keyof TypeMap['model']}>
+        ? PrismaFullModel<TypeMap, ModelName>
+        : never;
