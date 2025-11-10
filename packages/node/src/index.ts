e024d5a0413ae2cad067585158eaabddb93f15f4
electrovir <electrovir@users.noreply.github.com>
2025-11-10 16:05:04 +0000

    [patch] remove deprecated sanitizePath from @augment-vir/node, import it from @augment-vir/common instead

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -17,7 +17,6 @@ export * from './augments/path/contains.js';
 export * from './augments/path/os-path.js';
 export * from './augments/path/resolve-import.js';
 export * from './augments/path/root.js';
-export * from './augments/path/sanitize-path.js';
 export * from './augments/terminal/question.js';
 export * from './augments/terminal/relevant-args.js';
 export * from './augments/terminal/run-cli-script.js';
