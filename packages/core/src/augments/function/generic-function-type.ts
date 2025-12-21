d521d3f1722a26b1b887bc95bcbd276daf3cff1e
electrovir <electrovir@users.noreply.github.com>
2025-12-21 23:02:20 +0000

    [minor] add EmptyFunction, EmptyAsyncFunction, and EmptyMaybeAsyncFunction

--- packages/core/src/augments/function/generic-function-type.ts
+++ packages/core/src/augments/function/generic-function-type.ts
@@ -1,16 +1,44 @@
+import {type MaybePromise} from '../promise/maybe-promise.js';
+
 /**
- * A Function with no inputs and a return type of `Return` (which defaults to `any`).
+ * A function with no inputs and a return type of `Return` (which defaults to `any`).
  *
  * @category Function
  * @category Package : @augment-vir/common
  * @package [`@augment-vir/common`](https://www.npmjs.com/package/@augment-vir/common)
  */
 export type NoInputsFunction<Return = any> = () => Return;
+
 /**
- * A Function with any inputs and a return type of `Return` (which defaults to `any`).
+ * A function with any inputs and a return type of `Return` (which defaults to `any`).
  *
  * @category Function
  * @category Package : @augment-vir/common
  * @package [`@augment-vir/common`](https://www.npmjs.com/package/@augment-vir/common)
  */
 export type AnyFunction<Return = any> = (...args: any[]) => Return;
+
+/**
+ * A function with no inputs and no outputs.
+ *
+ * @category Function
+ * @category Package : @augment-vir/common
+ * @package [`@augment-vir/common`](https://www.npmjs.com/package/@augment-vir/common)
+ */
+export type EmptyFunction = () => void;
+/**
+ * An async function with no inputs and no outputs.
+ *
+ * @category Function
+ * @category Package : @augment-vir/common
+ * @package [`@augment-vir/common`](https://www.npmjs.com/package/@augment-vir/common)
+ */
+export type EmptyAsyncFunction = () => Promise<void>;
+/**
+ * A maybe async function with no inputs and no outputs.
+ *
+ * @category Function
+ * @category Package : @augment-vir/common
+ * @package [`@augment-vir/common`](https://www.npmjs.com/package/@augment-vir/common)
+ */
+export type EmptyMaybeAsyncFunction = () => MaybePromise<void>;
